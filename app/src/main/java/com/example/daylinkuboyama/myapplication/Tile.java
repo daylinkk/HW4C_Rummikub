36851cef05e75856f46d2622af57f90e3da07f30
Daylin K <daylinkuboyama@Daylin-Kuboyamas-MacBook-Pro.local>
2018-02-12 14:24:47 -0800

    Initial commit

---
+++ app/src/main/java/com/example/daylinkuboyama/myapplication/Tile.java
@@ -0,0 +1,78 @@
+package com.example.daylinkuboyama.myapplication;
+
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * class Tile
+ *
+ * This class draws Tiles; includes all basic framework.
+ *
+ * @author Daylin Kuboyama
+ * @author Harry Thoma
+ * @author Riley Snook
+ * @author Chris Lytle
+ */
+
+public class Tile {
+
+    //coordinates of tile
+    private int x;
+    private int y;
+
+    private int value; //value on tile
+    private int color; //tile's color
+
+    //sizing (width and height) of tile
+    public static final int WIDTH = 110;
+    public static final int HEIGHT = 120;
+
+    //4 tile colors
+    public static final int BLUE = Color.argb(255,0,0,255);
+    public static final int RED = Color.argb(255,255,0,0);
+    public static final int YELLOW = Color.argb(255,255,215,0);
+    public static final int GREEN = Color.argb(255,50,205,50);
+    //purpose of array is to assign numerical value to each color
+    public static final int[] colorArray = {BLUE, RED, YELLOW, GREEN};
+
+    /**
+     * When Tile is created, the coordinates of the tile, its value,
+     * and color should be specified.
+     */
+    public Tile (int tileX, int tileY, int value, int color) {
+        this.x = tileX;
+        this.y = tileY;
+        this.value = value;
+        this.color = color;
+    }
+
+    /**
+     * This method draws tiles using Canvas.
+     */
+    public void drawTile(Canvas c) {
+        //fills and outlines tile color
+        Paint tileColor = new Paint ();
+        tileColor.setARGB(255,255,250,250);
+        Paint tileOutline = new Paint ();
+        tileOutline.setStyle (Paint.Style.STROKE);
+        c.drawRect(x,y,x+WIDTH,y+HEIGHT,tileColor);
+        c.drawRect(x,y,x+WIDTH,y+HEIGHT,tileOutline);
+
+        //for the numbers that consist of 2 chars (ex: 10,11,12,13)
+        //moves the value printing more to the left
+        if (value > 9) {
+            Paint valColor = new Paint ();
+            valColor.setColor(color);
+            valColor.setTextSize(80.0f);
+            c.drawText(""+value, x+WIDTH/12,y+(HEIGHT*2)/3,valColor);
+        }
+        else {
+            Paint valColor = new Paint();
+            valColor.setColor(color);
+            valColor.setTextSize(80.0f);
+            c.drawText("" + value, x + WIDTH /3, y + (HEIGHT * 2) / 3, valColor);
+        }
+    }
+}
